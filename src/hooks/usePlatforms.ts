4ad75cfdcbd0ee0e5b4834a0f8beddef185e8bda
Ali Ahmad <aliahmad_997@outlook.com>
2023-11-05 18:16:58 +0500

    added react query and removed repetitve code

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,19 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient, { FetchRespone } from "../services/api-client";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+const usePlatform = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchRespone<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+  });
 
 export default usePlatform;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+export interface FetchRespone<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
